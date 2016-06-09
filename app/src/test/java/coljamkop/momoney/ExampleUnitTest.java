f67ea126ffd1388057dae4151a3101099f9df26f
emilio91 <deleon.cesar08@gmail.com>
2016-06-08 19:27:45 -0600

    wrote expenses class wrote test for expenses class

--- app/src/test/java/coljamkop/momoney/ExampleUnitTest.java
+++ app/src/test/java/coljamkop/momoney/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package coljamkop.momoney;
 
 import org.junit.Test;
 
+import coljamkop.momoney.dummy.Expenses;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,5 +13,19 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void There_isExpense() throws Exception{
+        Expenses exp = new Expenses(123.00);
+        assertEquals(exp.getTotal(), 123.00, 0);
+
+        Expenses exp1 = new Expenses(55.33);
+        assertEquals(exp1.getTotal(),55.33, 0);
+
+        Expenses exp2 = new Expenses(23.21);
+        assertEquals(exp2.getTotal(), 23.21, 0);
+
+
     }
 }

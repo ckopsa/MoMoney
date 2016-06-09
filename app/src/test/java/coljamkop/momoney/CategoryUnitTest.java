84a1ba018bdc4162b364cce74ac613a78a559577
emilio91 <deleon.cesar08@gmail.com>
2016-06-08 20:13:36 -0600

    erased expenses class created expense class created expensesUnitTest

--- app/src/test/java/coljamkop/momoney/CategoryUnitTest.java
+++ app/src/test/java/coljamkop/momoney/CategoryUnitTest.java
@@ -16,13 +16,13 @@ public class CategoryUnitTest {
         Category category = new Category(0.0, 20.0);
         // $0.00 <= $20.00
         assertEquals(true, category.isInGoal());
-        category.addExpense(new Expense(10.0, 0));
+        category.addExpense(new Expense(10.0));
         // $10.00 <= $20.00
         assertEquals(true, category.isInGoal());
-        category.addExpense(new Expense(10.0, 0));
+        category.addExpense(new Expense(10.0));
         // $20.00 <= $20.00
         assertEquals(true, category.isInGoal());
-        category.addExpense(new Expense(10.0, 0));
+        category.addExpense(new Expense(10.0));
         // $30.00 <= $20.00
         assertEquals(false, category.isInGoal());
     }
@@ -32,7 +32,7 @@ public class CategoryUnitTest {
         double total = 0;
         Category category = new Category(0.0, 20.0);
         for(int i = 0; i < 10; i++) {
-            category.addExpense(new Expense(10.0, 0));
+            category.addExpense(new Expense(10.0));
             total += 10.0;
             assertEquals(total, category.getTotal(), 0.0);
         }
--- app/src/test/java/coljamkop/momoney/ExpensesUnitTest.java
+++ app/src/test/java/coljamkop/momoney/ExpensesUnitTest.java
@@ -4,6 +4,9 @@ package coljamkop.momoney;
  * Created by emi_lion91 on 6/8/2016.
  */
 import org.junit.Test;
+
+import java.util.Date;
+
 import coljamkop.momoney.dummy.Expense;
 
 
@@ -21,5 +24,15 @@ public class ExpensesUnitTest {
 
         Expense exp2 = new Expense(23.21);
         assertEquals(exp2.getTotal(), 23.21, 0);
+    }
+
+    @Test
+    public void There_isDate() throws Exception{
+        Expense exp = new Expense(100.00);
+        Date d = new Date();
+        String date = d.toString();
+
+        assert(exp.getDate().equals(date));
+
     }
 }

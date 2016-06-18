0789c3c81a01a115ad883aafdb1877d415437c47
emilio91 <deleon.cesar08@gmail.com>
2016-06-18 12:13:49 -0600

    big data base and Current Month database Implemented

---
+++ app/src/main/java/coljamkop/momoney/dummy/Budget.java
@@ -0,0 +1,13 @@
+package coljamkop.momoney.dummy;
+
+import java.util.List;
+
+import coljamkop.momoney.Content.Month;
+
+/**
+ * Created by emi_lion91 on 6/10/2016.
+ */
+public class Budget {
+
+    private List<Month> months;
+}
